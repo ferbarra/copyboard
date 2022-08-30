c524823a35cfaba3802d88c779b470b4a2b3bada
Fernando Barra <barrarod@ualberta.ca>
2022-08-29 22:50:33 -0600

    Adds a couple of components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 
 function App() {
@@ -6,8 +7,16 @@ function App() {
       <header className="App-header">
         <h1>Copyboard</h1>
       </header>
+        <Entry copyValue="Hello, World!"></Entry>
+        <Entry copyValue="awijdiqjhdf"></Entry>
     </div>
   );
+}
+
+class Entry extends React.Component {
+    render() {
+        return <p>{this.props.copyValue}</p>
+    }
 }
 
 export default App;
